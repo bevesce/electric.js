bf92bdd3ab41a30b3472a587613c640025da56c4
Piotr Wilczyński <bevesce@gmail.com>
2015-08-19 12:15:02 +0200

    add utils

--- src/receivers/utils.ts
+++ src/receivers/utils.ts
@@ -2,11 +2,24 @@ export interface Node {
 	appendChild(child: any): void;
 }
 
-export function getNode(nodeOrId: Node | string): Node{
+type NodeOrId = Node | string;
+
+export function getNode(nodeOrId: NodeOrId): Node {
 	if (typeof nodeOrId === 'string'){
 		return document.getElementById(nodeOrId);
 	}
 	else {
 		return <Node>nodeOrId;
+	}
+}
+
+type NodesOrName = Node[]| string;
+
+export function getNodes(nodesOfName: NodesOrName): Node[] {
+	if (typeof nodesOfName === 'string') {
+		return Array.prototype.slice.call(document.getElementsByName(nodesOfName));
+	}
+	else {
+		return <Node[]>nodeOrId;
 	}
 }
